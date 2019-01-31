3397a65ffe9eceb7914954025e07ed4bbd4b94c4
Shayna Stewart <shaynakstewart@gmail.com>
2019-01-30 21:22:16 -0800

    Create hwOne.c
    initial file
    prints "Hello World"

---
+++ school/spring/CS256/hwOne.c
@@ -0,0 +1,14 @@
+/**
+ * Name: Shayna Stewart
+ * HW #1
+ * Due: 1/31/19
+ * Description: Program to output "Hello World"
+ */
+
+#include <stdio.h>
+
+
+int main(void) {
+	printf("Hello World\n");
+return 0;
+}
