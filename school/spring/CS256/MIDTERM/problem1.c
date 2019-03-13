65124ee8b2b74f949d6ea223d5b3a17b48fabc34
Shayna Stewart <shaynakstewart@gmail.com>
2019-03-12 17:51:13 -0700

    Update problem1.c

--- school/spring/CS256/MIDTERM/problem1.c
+++ school/spring/CS256/MIDTERM/problem1.c
@@ -1,1 +1,26 @@
+/******************************************************************************
+Name: Shayna Stewart
+Class: CS2560
+Description: Prints a multiplication table using the size entered by the user.
 
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int size = 0;
+    printf("Please enter a column/row size: ");
+    scanf("%d",&size);
+    
+    for(int i = 1; i < size+1; i++){ //double loop to assign 
+        for(int j = 1; j < size+1; j++){
+            printf("%d ",(i*j));
+            
+        }
+        printf("\n");
+        
+    }
+
+    return 0;
+}
