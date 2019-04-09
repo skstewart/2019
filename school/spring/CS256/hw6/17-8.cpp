fe3f9756aaa07ed721c080a2202a5600757eef11
Shayna Stewart <shaynakstewart@gmail.com>
2019-04-09 09:18:39 -0700

    Update 17-8.cpp

--- school/spring/CS256/hw6/17-8.cpp
+++ school/spring/CS256/hw6/17-8.cpp
@@ -1,63 +1,65 @@
 #include <iostream> 
 
-#include <vector> 
-
-#include <string> 
-
 using namespace std;
 
-int remove_if(int*, int);
+int remove(int*, int);
 
 int main() {
 	int n;
-	std::cout << "Enter an integer greater than 1: ";
-	std::cin >> n;
-	int* N = (int*)malloc((n - 1) * sizeof(int));
+	cout << "Enter an integer greater than 1: ";
+	cin >> n;
+	int* N = new int[1024]; // allocating space
 	int i, j;
 
-	for (i = 2; i <= n; i++) {
-		N[i = 2] = i;
+	for (i = 2; i <= n; i++) { //creating our array of numbers beginning at 2 (1 is not prime)
+		N[i - 2] = i;	
 	
 	}
 
-	cout << "Array before removal of composite numbers:" << endl;
-	for (i = 0; i < n; i++) {
-		cout << N[i] << endl;
+	cout << "Entire array before removal of composite numbers:" << endl;
+	for (i = 0; i < n-1; i++) {
+		cout << N[i] << " ";	//print initial array
 	}
+	cout << endl;
 
-	j = remove_if(&N[0], n);
+	j = remove(&N[0], n);	//send to remove function
+
 	cout << "Array after removal of composite numbers:" << endl;
 	for (i = 0; i < j - 1; i++) {
-		cout << *(N + i) << endl;
+		cout << *(N + i) << " ";
 	}
 
 	cout << endl;
 
+	delete[] N;	//delete from memory 
+
+	return 0;
+
 }
 
-int remove_if(int *N, int size) {
+int remove(int *N, int size) {
 	int i, j;
-	int flag, comp_i;
+	int toDelete;
+	bool isComposite;
 
 	for (i = 0; i < size; i++) {
-		flag = 0;
-		for (j = 2; j < i; j++) {
+		isComposite = false;
+		for (j = 2; j < i+2; j++) { //if a number is divisible by anything but 1, it is composite.
 			if (N[i] % j == 0){
-				flag = 1;
-				comp_i = i;
+				isComposite = true; //if composite, mark 
+				toDelete = i; //mark 
 			}
 		}
-		if (flag == 1) {
-			N[comp_i] = -1;
+		if (isComposite == true) { //if marked as composite,
+			N[toDelete] = -1; //mark for removal
 		}
 	}
 	j = 0;
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size+1; i++) { //if marked, remove
 		if (N[i] != -1) {
 			N[j++] = N[i];
 		}
 	
 	}
-	N = (int*)realloc(N, sizeof(int)*j);
 	return j;
 }
