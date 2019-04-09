4b061c8eaf76ac9bc37d945e3f79fcb3d507f263
Shayna Stewart <shaynakstewart@gmail.com>
2019-04-08 22:16:32 -0700

    Update 16-3.cpp

--- school/spring/CS256/hw6/16-3.cpp
+++ school/spring/CS256/hw6/16-3.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
+#include "pch.h"
 using namespace std;
 
 template <typename T>
-inline T const& Max (T const& firstData, T const& secondData)
+inline T const& Max(T const& firstData, T const& secondData)
 {
-return firstData < secondData ? secondData:firstData;
+	return firstData < secondData ? secondData : firstData;
 }
 template <typename T>
-inline T const& Min (T const& firstData, T const& secondData)
+inline T const& Min(T const& firstData, T const& secondData)
 {
-return firstData > secondData ? secondData:firstData;
+	return firstData > secondData ? secondData : firstData;
 }
 
 int main()
 {
-cout<<"Max(1, 24) is: "<< Max(1,24) <<endl;
-cout<<"Max(-26, -12) is: "<< Max(-26, -12) <<endl;
-cout<<"Max(123.456, 789.123) is: "<< Max(123.456, 789.123) <<endl;
-cout<<"Max(1234, 1111) is: "<< Max(1234, 1111) <<endl;
-cout<<"Max(shayna, Shayna) is: "<< Max("shayna", "Shayna") <<endl;
-cout<<"Max(London, Tokyo) is: "<< Max("London", "Tokyo") <<endl;
-  
-cout<<"Min(1, 24) is: "<< Min(1,24) <<endl;
-cout<<"Min(-26, -12) is: "<< Min(-26, -12) <<endl;
-cout<<"Min(123.456, 789.123) is: "<< Min(123.456, 789.123) <<endl;
-cout<<"Min(1234, 1111) is: "<< Min(1234, 1111) <<endl;
-cout<<"Min(shayna, Shayna) is: "<< Min("shayna", "Shayna") <<endl;
-cout<<"Min(London, Tokyo) is: "<< Min("London", "Tokyo") <<endl;
+	cout << "Max(1, 24) is: " << Max(1, 24) << endl;
+	cout << "Max(-26, -12) is: " << Max(-26, -12) << endl;
+	cout << "Max(123.456, 789.123) is: " << Max(123.456, 789.123) << endl;
+	cout << "Max(1234, 1111) is: " << Max(1234, 1111) << endl;
+	cout << "Max(shayna, Shayna) is: " << Max("shayna","Shayna") << endl;
+
+
+	cout << "Min(1, 24) is: " << Min(1, 24) << endl;
+	cout << "Min(-26, -12) is: " << Min(-26, -12) << endl;
+	cout << "Min(123.456, 789.123) is: " << Min(123.456, 789.123) << endl;
+	cout << "Min(1234, 1111) is: " << Min(1234, 1111) << endl;
+	cout << "Min(shayna, Shayna) is: " << Min("shayna", "Shayna") << endl;
 }
