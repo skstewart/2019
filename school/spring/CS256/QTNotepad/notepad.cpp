7558b010c518da62cf1ca6266154736336d76d49
Shayna Stewart <shaynakstewart@gmail.com>
2019-05-04 17:20:48 -0700

    Update notepad.cpp

--- school/spring/CS256/QTNotepad/notepad.cpp
+++ school/spring/CS256/QTNotepad/notepad.cpp
@@ -1,3 +1,10 @@
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QFont>
+#include <QFontDialog>
+
 #include "notepad.h"
 #include "ui_notepad.h"
 
@@ -6,9 +13,113 @@ Notepad::Notepad(QWidget *parent) :
     ui(new Ui::Notepad)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+
+    connect(ui->actionNew, &QAction::triggered, this, &Notepad::newDocument);
+    connect(ui->actionOpen, &QAction::triggered, this, &Notepad::open);
+    connect(ui->actionSave, &QAction::triggered, this, &Notepad::save);
+    connect(ui->actionExit, &QAction::triggered, this, &Notepad::exit);
+    connect(ui->actionCopy, &QAction::triggered, this, &Notepad::copy);
+    connect(ui->actionCut, &QAction::triggered, this, &Notepad::cut);
+    connect(ui->actionPaste, &QAction::triggered, this, &Notepad::paste);
+    connect(ui->actionUndo, &QAction::triggered, this, &Notepad::undo);
+    connect(ui->actionRedo, &QAction::triggered, this, &Notepad::redo);
+
+
+
+#if !QT_CONFIG(clipboard)
+    ui->actionCut->setEnabled(false);
+    ui->actionCopy->setEnabled(false);
+    ui->actionPaste->setEnabled(false);
+#endif
 }
 
 Notepad::~Notepad()
 {
     delete ui;
 }
+
+void Notepad::newDocument()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void Notepad::open()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void Notepad::save()
+{
+    QString fileName;
+    // If we don't have a filename from before, get one.
+    if (currentFile.isEmpty()) {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    } else {
+        fileName = currentFile;
+    }
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+
+
+
+void Notepad::exit()
+{
+    QCoreApplication::quit();
+}
+
+void Notepad::copy()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->copy();
+#endif
+}
+
+void Notepad::cut()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->cut();
+#endif
+}
+
+void Notepad::paste()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->paste();
+#endif
+}
+
+void Notepad::undo()
+{
+     ui->textEdit->undo();
+}
+
+void Notepad::redo()
+{
+    ui->textEdit->redo();
+}
+
+
