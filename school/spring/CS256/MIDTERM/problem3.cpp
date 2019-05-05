11007815b614128e2ffb7ef8d8b77412bcb148b1
Shayna Stewart <shaynakstewart@gmail.com>
2019-05-05 11:56:19 -0700

    Update problem3.cpp

--- school/spring/CS256/MIDTERM/problem3.cpp
+++ school/spring/CS256/MIDTERM/problem3.cpp
@@ -1,70 +1,77 @@
-#include <iostream>
 
-class CreateArray {
-	int findMin(float *arr, int n);
-	int findMax(float *arr, int n);
+#include <iostream>
+using namespace std;
+
+
+
+	int findMin(float arr[], int n) {
+		int min = arr[0];
+
+		for (int x = 0; x < n; x++) {
+			if (arr[x] < min) {
+				min = arr[x];
+			}
+
+
+		} //end loop
+		return min;
+	}
+
+	int findMax(float arr[], int n) {
+		int max = -1;
+		for (int x = 0; x < n; x++) {
+
+			if (arr[x] > max) {
+				max = arr[x];
+			}
+
+		} //end loop
+		return max;
+	}
 
 	int main() {
 		float * arr = NULL;
 		int userChoice = -1;
 		int size = -1;
 
-		std::cout << "Please enter an array size:";
-		std::cin >> size;
+		cout << "Please enter an array size:" << endl;
+		cin >> size;
 		arr = new float[size];
+		for (int i = 0; i < size; i++)
+			arr[i] = 0;
 
-		std::cout << "Menu: \n1. Store a number in the array\n2. Retrieve the value of an element of the array.";
-		std::cout << "\n3. Retrieve the highest value in the array.\n4. Retrieve the lowest value in the array.\n";
-		std::cin >> userChoice;
+	
+		while (true) {
+			cout << "Menu: \n1. Store a number in the array\n2. Retrieve the value of an element of the array.";
+			cout << "\n3. Retrieve the highest value in the array.\n4. Retrieve the lowest value in the array.\n5. Exit" << endl;
+			cin >> userChoice;
+			cout << endl;
 
-		switch (userChoice) {
-		case 1: { int n, value;
-			std::cout << "Please enter an element number";
-			std::cin >> n;
-			std::cout << "Please enter a value to store";
-			std::cin >> value;
-			arr[n] = value;
+			switch (userChoice) {
+			case 1: { int n, value;
+				cout << "Please enter an element number: ";
+				cin >> n;
+				cout << "Please enter a value to store: ";
+				cin >> value;
+				arr[n] = value;
 
-		}break;
-		case 2: {int n, value;
-			std::cout << "Please enter an element number";
-			std::cin >> n;
-			std::cout << arr[n];
-		case 3: {
-			std::cout << "The highest value is: " << findMax(arr, size) << "";
+			}break;
+			case 2: {int n, value;
+				cout << "Please enter an element number: ";
+				cin >> n;
+				cout << arr[n];
+			case 3: {
+				cout << "The highest value is: " << findMax(arr, size) << endl;
 
-		}break;
-		case 4: {
-			int min = findMin(arr, size);
-		}break;
+			}break;
+			case 4: {
+				int min = findMin(arr, size);
+				cout << "The lowest value is: " << min << endl;
+			}break;
+			case 5: {exit(0); }
+			}
+
+					delete arr;
+			}
 		}
-
-				delete arr;
-		}
-
-		int findMin(float *arr, int n) {
-			int min = -1;
-
-			for (int x = 0; x < n; x++) {
-				if (&arr[x] < min) {
-					min = &arr[x];
-				}
-
-
-			} //end loop
-			return min;
-		}
-
-		int findMax(float *arr, int n) {
-			int max = -1;
-			for (int x = 0; x < n; x++) {
-
-				if (&arr[x] > max) {
-					max = &arr[x];
-				}
-
-			} //end loop
-			return max;
-		}
-
 	}
