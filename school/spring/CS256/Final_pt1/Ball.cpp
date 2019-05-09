ecdcfbd98a739003ac1b2236c19a6a6d91039245
Shayna Stewart <shaynakstewart@gmail.com>
2019-05-08 21:34:40 -0700

    Update Ball.cpp

--- school/spring/CS256/Final_pt1/Ball.cpp
+++ school/spring/CS256/Final_pt1/Ball.cpp
@@ -1,6 +1,5 @@
 #include "Ball.h"
-
-// This the constructor and it is called when we create an object
+//constructor
 Ball::Ball(float startX, float startY)
 {
 	position.x = startX;
@@ -44,10 +43,8 @@ void Ball::hitBottom()
 
 void Ball::update()
 {
-	// Update the ball position variables
 	position.y += yVelocity;
 	position.x += xVelocity;
 
-	// Move the ball and the bat
 	ballShape.setPosition(position);
 }
